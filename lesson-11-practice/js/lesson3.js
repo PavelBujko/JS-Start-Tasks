657a18ee8eaf245cea20c4aa75d4ceacb3331be5
PavelBujko <81901247+PavelBujko@users.noreply.github.com>
2022-01-30 11:16:01 +0300

    Update lesson3.js

--- lesson-11-practice/js/lesson3.js
+++ lesson-11-practice/js/lesson3.js
@@ -1,19 +1,21 @@
+const hour = document.querySelector('.hour')
+const min = document.querySelector('.min');
+const sec = document.querySelector('.sec');
+const semion = document.querySelectorAll('.semicolon')
 var intervalID = window.setInterval(restartTime, 1000);
-
-let div = document.createElement('div');
-document.body.appendChild(div);
 
 function restartTime() {
     var now = new Date().toLocaleTimeString();
-    console.log(now);
 
     let nowH = now.slice(0,2);
+    let semicolon = now.slice(2,3);
     let nowM = now.slice(3,5);
     let nowS = now.slice(6,8);
 
-    let nowHMS = nowH + ':' + nowM + ':' + nowS;
-
-    div.innerText = nowHMS;
+    hour.innerText = nowH;
+    semion[0].innerText = semicolon;
+    min.innerText = nowM;
+    semion[1].innerText = semicolon;
+    sec.innerText = nowS;
 }
-
 
