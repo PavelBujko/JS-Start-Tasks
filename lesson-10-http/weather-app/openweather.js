254d35d3499a0d10525a934373fabe31f567d728
PavelBujko <81901247+PavelBujko@users.noreply.github.com>
2022-01-30 10:51:52 +0300

    Update openweather.js

--- lesson-10-http/weather-app/openweather.js
+++ lesson-10-http/weather-app/openweather.js
@@ -1,29 +1,15 @@
 function createIcon(value){
-    let links =  "http://openweathermap.org/img/wn/10d@2x.png";
-    let split = links.split('')
-    
-    split.splice(33, 3, value)
-
-    let result = split.join(''); 
+    let result = `http://openweathermap.org/img/wn/${value}@2x.png`
     return result
 }
 
-const current = document.querySelector('.current')
-const forecast = document.querySelector('.forecast')
+const current = document.querySelector('.current');
+const forecast = document.querySelector('.forecast');
 
-function createElemInCurrent(tegName, content, nameClass){
+function createElem(tegName, content, nameClass, parent){
     let teg = document.createElement(tegName);
     teg.innerText = content;
-    current.appendChild(teg);
-
-    teg.classList.add(nameClass);
-}
-
-function createElemInforecast(tegName, content, nameClass){
-    let teg = document.createElement(tegName);
-    teg.innerText = content;
-    forecast.appendChild(teg);
-
+    parent.appendChild(teg);
     teg.classList.add(nameClass);
 }
 
@@ -33,12 +19,12 @@ function getForecast(linkAPI){
     httpRequest.onload = function(){
     
         let country = JSON.parse(httpRequest.responseText);
-        createElemInCurrent("p", country.city.name, "country") //Country Name
+        createElem("p", country.city.name, "country", current)
     
         //-----------------//
     
         let data = JSON.parse(httpRequest.responseText);
-        createElemInCurrent("p", data.list[0].dt_txt, "time") //TIME
+        createElem("p", data.list[0].dt_txt, "time", current) //TIME
     
         //-----------------//
     
@@ -56,18 +42,16 @@ function getForecast(linkAPI){
         //-----------------//
     
         let weather = JSON.parse(httpRequest.responseText);
-        createElemInCurrent("H2", weather.list[0].main.temp, "weather") //Weather
+        createElem("H2", Math.round(weather.list[0].main.temp), "weather", current)//Weather
     
         //-----------------//
        //-----------------//
     
        for(let i = 0; i < 40; i +=8){
-    
-           createElemInforecast("div", '', "row")
+           createElem("div", '', "row", forecast)
     
             let weather = JSON.parse(httpRequest.responseText);
-            createElemInforecast("p", weather.list[i].main.temp, "weatherforecast")
-    
+            createElem("p", Math.round(weather.list[i].main.temp), "weatherforecast", forecast);
             let icon = JSON.parse(httpRequest.responseText);
             let iconId = icon.list[i].weather[0].icon;
     
@@ -81,7 +65,7 @@ function getForecast(linkAPI){
     
     
             let data = JSON.parse(httpRequest.responseText);
-            createElemInforecast("p", data.list[i].dt_txt, "time")
+            createElem("p", data.list[i].dt_txt, "time", forecast);
        }
     
     }
@@ -93,15 +77,13 @@ function getForecast(linkAPI){
 
 navigator.geolocation.getCurrentPosition(
     function(position) {
-	    alert('Последний раз вас засекали здесь: ' +
-		    position.coords.latitude + ", " + position.coords.longitude);
+	    // alert('Последний раз вас засекали здесь: ' +
+		//     position.coords.latitude + ", " + position.coords.longitude);
 
                 let lat = position.coords.latitude;
                 let lon = position.coords.longitude;
     
                 let link = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=248dee22376dff44b5f737f243fc02c6`
-
-                console.log(link)
                 getForecast(link)
 	}
 );
