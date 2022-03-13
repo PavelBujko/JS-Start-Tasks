53d17283cc9c9f94c1da2a3022fa87af2ffdeb71
PavelBujko <81901247+PavelBujko@users.noreply.github.com>
2022-03-13 12:02:50 +0300

    Update map.js

--- lesson-14-Headers-and-methods-of-HTTP-requests-Fetch/js/map.js
+++ lesson-14-Headers-and-methods-of-HTTP-requests-Fetch/js/map.js
@@ -15,8 +15,8 @@ function CreateCards(flag, countryName, region, population, languageStr, moneySt
     container.innerHTML += cars;
 }
 
-function returnData(data){
-    for (let i = 0; i < 120; i+=10) {
+function returnData(data) {
+    for (let i = 0; i < 120; i += 10) {
         let flag = data[i].flags.png;
         let countryName = data[i].name.common;
         let region = data[i].region;
@@ -24,10 +24,13 @@ function returnData(data){
         let money = data[i].currencies
         let langu = data[i].languages;
 
+
         let languageStr = []
         for (let prop in langu) {
             languageStr.push(langu[prop])
         }
+
+        populationRound = String(Math.round(population - 0.1).toFixed(1))
 
         let moneyStr = []
         for (let prop in money) {
@@ -35,7 +38,14 @@ function returnData(data){
         }
 
 
-        CreateCards(flag, countryName, region, population, languageStr, moneyStr)
+        if ((populationRound.length - 2) === 9 || (populationRound.length - 2) === 8 || (populationRound.length - 2) === 7) {
+            let popul1 = Math.round(Math.round(populationRound) / 1000000) + ' млн'
+            CreateCards(flag, countryName, region, popul1, languageStr.join(', '), moneyStr)
+            
+        } else{
+            let popul2 = Math.round(Math.round(populationRound) / 1000) + ' тыс'
+            CreateCards(flag, countryName, region, popul2, languageStr.join(', '), moneyStr)
+        }
     }
 }
 
@@ -47,5 +57,3 @@ fetch('https://restcountries.com/v3.1/all')
     .then((data) => {
         returnData(data)
     });
-
-
